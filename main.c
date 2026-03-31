583bd4f1d7a3eb9ed988771751e66ac2d7c7bcad
slp227 <slp227@lehigh.edu>
2026-03-31 11:42:18 -0400

    start files and add libraries

---
+++ main.c
@@ -0,0 +1,31 @@
+//******************ECE 132*************************
+//Names: Kasey White and Sophia Pham
+//Lab section: Tuesday 1:35
+//*************************************************
+//Date Started: 3/31/2026
+//Date of Last Modification:
+//Assignment: Proj. 1
+//*************************************************
+//Purpose of program:
+//Program Inputs:
+//Program Outputs:
+//*************************************************
+
+//File include Statements
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "driverlib/adc.h"
+
+void main() {
+    
+}
